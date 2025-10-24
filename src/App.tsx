c5ce106a8e58ad734de910102f78b7c74e5ec542
OlaOluwalekan <olalekanbello534@gmail.com>
2025-10-24 15:18:11 +0100

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-100 p-8'>
+      <h1 id='home-title' className='text-3xl font-bold text-blue-600'>
+        Welcome to Cypress Demo ✅
+      </h1>
+
+      <p className='mt-4 text-gray-700'>This is our testing playground 🎯</p>
+    </div>
+  )
+}
